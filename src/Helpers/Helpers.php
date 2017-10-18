47dd65bf0d0565a111faccea7b3b16ba34fc1095
pecwu <pecwu@patazon.com>
2017-10-18 14:48:59 +0800

    add assetwith version

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -18,3 +18,23 @@ if(! function_exists('trimArray')) {
         return array_map('trimArray', $datas);
     }
 }
+
+if(! function_exists('assetWithVersion')) {
+    /**
+     * 给资源文件生成版本信息, 版本依据文件修改时间
+     * 
+     * @param string $path
+     * 
+     * @return string
+     */
+    function assetWithVersion($path)
+    {
+        $filePath = public_path($path);
+
+        if (\File::exists($filePath)) {
+            $time = \File::lastModified($path);
+            return asset($path) . '?v=' . $time;
+        }
+        return asset($path);
+    }
+}
